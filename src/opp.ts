2ecc44c55c94c0ed3acaa115938298253022ac34
wahidul islam sami <wahidulislamsami5@gmail.com>
2026-01-22 23:45:17 +0600

    opps p-1

---
+++ src/opp.ts
@@ -0,0 +1,41 @@
+// class chai {
+//     flavour: string;
+//     price:number;
+
+// constructor(flavour:string , price: number ) {
+//     this.flavour = flavour;
+//     this.price = price;
+// }
+// }
+
+
+// const maslachai = new chai("ginger" , 10)
+// maslachai.flavour = "masala"
+
+
+class chai {
+    public flavour :string = "masala"
+private secretinergeadint   =  "cardmom" 
+
+reveal() {
+    return this.secretinergeadint 
+}
+
+
+
+}
+
+
+
+// class shop {
+// protected shopName  =  "chai sami "
+// } 
+
+// class branch extends shop {
+//     getname() {
+//         return( this.shopName)
+//     }
+// }
+
+const c  = new chai()
+// c.reveal()
