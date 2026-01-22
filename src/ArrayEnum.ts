b6e6231ff3adb2c66158018984fd143efac03906
wahidul islam sami <wahidulislamsami5@gmail.com>
2026-01-22 23:17:29 +0600

    ArrayEnum ts  added

---
+++ src/ArrayEnum.ts
@@ -0,0 +1,24 @@
+const chiaFlavours : string[] =["masala , tea" , "adark"]
+const  chaiPrice: number[] = [10, 20]
+
+const rating  : Array<number> = [4.5 , 5.0 ]
+
+type chai = {
+    name: string;
+    price : number
+}
+
+ const menu: chai[] = [
+    {name: "masala", price: 15},
+    {name: "tea", price: 5}
+ ] 
+//  menu.push({name: "tea", price: 5})
+
+ const citees : readonly string[] =[  "chattogram" , "dhaka" ]
+//  citees.push("pune")
+
+const table: number[][]  =  [
+    [1 ,2 , 3],
+    [4 , 5, 6]
+]
+
