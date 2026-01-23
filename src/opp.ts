0aa1bfef6d573e1b6f02b5172bb889933f94a229
wahidul islam sami <wahidulislamsami5@gmail.com>
2026-01-23 16:46:19 +0600

    opp.ts new  version

--- src/opp.ts
+++ src/opp.ts
@@ -8,34 +8,96 @@
 // }
 // }
 
-
 // const maslachai = new chai("ginger" , 10)
 // maslachai.flavour = "masala"
 
-
 class chai {
-    public flavour :string = "masala"
-private secretinergeadint   =  "cardmom" 
+  public flavour: string = "masala";
+  private secretinergeadint = "cardmom";
 
-reveal() {
-    return this.secretinergeadint 
+  reveal() {
+    return this.secretinergeadint;
+  }
 }
 
-
-
-}
-
-
-
-// class shop {
-// protected shopName  =  "chai sami "
-// } 
-
-// class branch extends shop {
-//     getname() {
-//         return( this.shopName)
-//     }
-// }
-
-const c  = new chai()
+const c = new chai();
 // c.reveal()
+
+class shop {
+  protected shopName = "chai sami ";
+}
+
+class branch extends shop {
+  getname() {
+    return this.shopName;
+  }
+}
+
+class Wallet {
+  #balance = 1100;
+
+  getBalance() {
+    return this.#balance;
+  }
+}
+
+const w = new Wallet();
+
+// readONly
+
+class Cup {
+  readonly capacity: number = 270;
+
+  constructor(capacity: number) {
+    this.capacity = capacity;
+  }
+}
+
+// getter  and  suter  method
+
+class modernChai {
+  private _sugar = 2;
+
+  get sugar() {
+    return this._sugar;
+  }
+
+  set sugar(value: number) {
+    if (value > 5) throw new Error("is  to late");
+    this._sugar = value;
+  }
+}
+
+const d = new modernChai();
+d.sugar = 4;
+
+class chaidaw {
+  static shopName = "samicode caffe";
+
+  constructor(public flavour: string) {}
+}
+
+console.log(chaidaw.shopName);
+
+abstract class Drink {
+  abstract make(): void;
+}
+
+class amrchai extends Drink {
+  make() {
+    console.log("brewing  chai");
+  }
+}
+
+
+class  heater {
+    heat(){}
+}
+
+class chaiMaker {
+    constructor(private heater: heater){}
+
+make(){
+    this.heater.heat
+}
+}
