c4c333c88a3671efe4a079bc8fc1b5f02b5e66c0
wahidul islam sami <wahidulislamsami5@gmail.com>
2026-01-24 23:01:28 +0600

    Genrics added

---
+++ src/Genrics.ts
@@ -0,0 +1,48 @@
+function wrapInArray<T>(item: T):  T[]{
+    return [item]
+} 
+
+wrapInArray("masala")
+wrapInArray(21)
+wrapInArray({flavour: 'ginger'})
+
+
+
+
+// ? TypeScript-এ Generics হলো এমন একটি পাওয়ারফুল ফিচার যা আপনাকে রিব্যবজেবল (Reusable)
+//  ? এবং টাইপ-সেফ (Type-safe) কোড লিখতে সাহায্য করে। সহজ কথায়, কোনো ফাংশন বা ক্লাস তৈরি 
+// ? করার সময় যদি আপনি আগে থেকে না জানেন যে সেখানে কী ধরনের ডেটা (Number, String,
+// ? নাকি Object) আসবে, তখন আপনি Generics ব্যবহার করেন।
+
+
+
+function pair <A , B>(a: A , b: B): [A , B] {
+    return [a, b]
+    // return [B , A]
+}
+
+pair("masla" , 20)
+pair("masala" , {flabor:  "ginger"})
+
+
+interface box <y> {
+    content : y 
+}
+
+const numberBox: box<number> = {content: 10}
+//*  const numberBoxTwo: box<string> = {content: 10} 
+
+//! real  uses  Genraics
+
+interface ApiResponse<T>{
+    status: number,
+    data:T
+}
+
+
+const res: ApiResponse<{flavor: string}> = {
+    status: 200,
+    data: { flavor: "masala" }
+};
+
+
